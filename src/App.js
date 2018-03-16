d4f855922051567ef1e43bfbd95b628dfa952354
alexfigtree <alexandra.higuera@gmail.com>
2018-03-16 17:25:07 -0400

    Port front end

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
 import getWeb3 from './utils/getWeb3'
 
+import './css/home.css'
 import './css/oswald.css'
 import './css/open-sans.css'
 import './css/pure-min.css'
@@ -75,15 +76,23 @@ class App extends Component {
         </nav>
 
         <main className="container">
-          <div className="pure-g">
-            <div className="pure-u-1-1">
-              <h1>Good to Go!</h1>
-              <p>Your Truffle Box is installed and ready.</p>
-              <h2>Smart Contract Example</h2>
-              <p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
-              <p>Try changing the value stored on <strong>line 59</strong> of App.js.</p>
-              <p>The stored value is: {this.state.storageValue}</p>
+          
+          <div className="home">
+            <div className="header">Transparency TCR List page</div>
+            <div className="list">
+              <hr/>
+              <li>Entry1</li>
+              <hr/>
+              <li>Entry2</li>
+              <hr/>
+              <li>Entry3</li>
+              <hr/>
+              <li>Entry4</li>
             </div>
+            <div className="header">
+              <button >VOTE PAGE</button>
+            </div>
+
           </div>
         </main>
       </div>
